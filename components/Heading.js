c8ba68a1476eeed86b18091ddccc83391f89e129
nctfall <eric_ivhivp@outlook.com>
2025-02-08 11:57:22 -0500

    push

--- components/Heading.js
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native'
-import Colors from '../utils/Colors'
-Colors
-
-export default function Heading({ title }) {
-  return (
-    <Text style={{ fontSize: 19, color: Colors.BLACK, fontWeight: "bold" }}>{title}</Text>
-  )
-}
